9b96a5c4f2851f15345031dc6b5cf018c7895bc6
Madhuka Methsara <methsaramadhuka@gmail.com>
2026-08-26 07:32:15 +0530

    remove backend structure

--- server/src/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDB() {
-  const uri = process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/loom';
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
-  console.log(`[db] connected to ${uri}`);
-}
